80fdf936b3950fdba9f3ab48ff638b8625713a68
Jose3533 <kosei.matsuoka@aiesec.net>
2024-07-08 23:33:31 -0300

    1

--- pages/4_sample.py
+++ pages/4_sample.py
@@ -7,6 +7,7 @@ from PIL import Image
 from mtcnn import MTCNN
 import streamlit as st
 import matplotlib.pyplot as plt
+from utils.grad_cam import GradCAM  # utilsフォルダに移動したGrad-CAMクラスのインポート
 
 # ディレクトリのベースパスを取得
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -18,50 +19,6 @@ st.set_page_config(
 
 # サイドバーに "Brew"
 st.sidebar.write("Brew")
-
-# utils/grad_cam.py から GradCAM クラスのコードを直接組み込む
-class GradCAM:
-    def __init__(self, model, target_layer):
-        self.model = model
-        self.target_layer = target_layer
-        self.gradients = None
-        self.activations = None
-        self._register_hooks()
-
-    def _register_hooks(self):
-        def forward_hook(module, input, output):
-            self.activations = output.detach()
-
-        def backward_hook(module, grad_in, grad_out):
-            self.gradients = grad_out[0].detach()
-
-        self.target_layer.register_forward_hook(forward_hook)
-        self.target_layer.register_backward_hook(backward_hook)
-
-    def generate_cam(self, input_tensor, class_idx=None):
-        if class_idx is None:
-            class_idx = input_tensor.argmax(dim=1).item()
-
-        self.model.zero_grad()
-        input_tensor.requires_grad = True
-
-        output = self.model(input_tensor)
-        target = output[0, class_idx]
-        target.backward()
-
-        gradients = self.gradients
-        activations = self.activations
-
-        pooled_gradients = torch.mean(gradients, dim=[0, 2, 3])
-        for i in range(activations.shape[1]):
-            activations[:, i, :, :] *= pooled_gradients[i]
-
-        heatmap = torch.mean(activations, dim=1).squeeze()
-        heatmap = np.maximum(heatmap.cpu().numpy(), 0)
-        heatmap = cv2.resize(heatmap, (input_tensor.shape[2], input_tensor.shape[3]))
-        heatmap = heatmap - np.min(heatmap)
-        heatmap = heatmap / np.max(heatmap)
-        return heatmap
 
 # モデルを読み込む関数
 def load_model(model_path, num_classes=5):
